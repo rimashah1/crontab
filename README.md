2dcc3f2e9ee95448f91a1f4c5803019aa6fca3d6
Rima Shah <110838409+rimashah1@users.noreply.github.com>
2022-10-04 14:11:51 -0400

    Updates

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # crontab
+
+1. Set up and start virtual machine
+2. ssh user@public_ip
+3. Password
+4. sudo apt-get update
+5. select-editor --> 1 for nano
+6. git clone url
+7. cd into folder
+8. crontab -e --> add cron expression 
+    ex. * * * * * usr/bin/python3 pwd path
+9. sudo service cron reload (to restart)
+
+# Execute file once a day
+23 16 * * * /usr/bin/python3 /home/rima/crontab/crontab.py
+
+# Execute file every Sunday at 10pm
+0 20 * * SUN /usr/bin/python3 /home/rima/crontab/crontab.py
+
+# Execute file at the end of every quarter
+0 0 1 */3 * /usr/bin/python3 /home/rima/crontab/crontab.py
--- crontab.py
+++ crontab.py
@@ -2,20 +2,25 @@ import os
 import sys
 import time
 import requests
-
-# request data
-df = requests.get('https://covid-api.mmediagroup.fr/v1/cases')
-df = df.json()
+import pandas as pd
 
 # get the current time
 now = time.time()
 
 # save current time as a string
-nowStr = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime(now))
+nowStr = time.strftime("%Y-%m-%d_%H%M", time.localtime(now)) # year-month-day hour-minute
 
-# create a new file in the current working directory
-with open('/home/rima/crontab/Covid' + nowStr + '.txt', 'w') as f:
-    f.write(str(df))
+# request data
+data = requests.get('https://covid-api.mmediagroup.fr/v1/cases')
+data = data.json() # load data as json
+df = pd.DataFrame.from_dict(data) # load into pandas df
+df.to_csv('data/covid' + nowStr + '.csv') # save data as .csv locally into data folder
+
+
+## create a new file in the current working directory
+# this saves data in the virtual machine
+with open('/home/rima/crontab/covid' + nowStr + '.csv', 'w') as f:
+    f.write(str(df)) 
 
 
 
